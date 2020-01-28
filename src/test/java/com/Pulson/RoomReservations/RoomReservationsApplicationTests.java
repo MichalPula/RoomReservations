fed07cf201f173af4f1c644a61ba53bd034e3d25
pulson <michal.pula98@gmail.com>
2020-01-28 12:00:17 +0100

    Initial commit.

---
+++ src/test/java/com/Pulson/RoomReservations/RoomReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pulson.RoomReservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
