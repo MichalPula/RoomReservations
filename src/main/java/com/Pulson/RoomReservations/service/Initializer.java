35534414f7ab3acde0411294b08d06eb2e79751d
pulson <michal.pula98@gmail.com>
2020-01-29 21:55:41 +0100

    Add delete() method.

--- src/main/java/com/Pulson/RoomReservations/service/Initializer.java
+++ src/main/java/com/Pulson/RoomReservations/service/Initializer.java
@@ -9,7 +9,7 @@ public class Initializer {
     public Initializer(UserRepository userRepository){
         userRepository.save(new User("LeBron", "James", "lebron@gmail.com", 2));
         userRepository.save(new User("Kevin", "Durant", "kd@gmail.com", 1));
-        userRepository.save(new User("Michael", "Duke", "steph_curry@gmail.com", 2));
+        userRepository.save(new User("Stephen", "Curry", "steph_curry@gmail.com", 2));
         userRepository.save(new User("Kyrie", "Irving", "kyrie_irvingh@gmail.com", 1));
         userRepository.save(new User("Derrick", "Rose", "drose@gmail.com", 2));
     }
