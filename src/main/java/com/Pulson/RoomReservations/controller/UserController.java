a3edf3d367b90609736b64b32fd343a5f6f7a265
pulson <michal.pula98@gmail.com>
2020-01-29 14:31:41 +0100

    Add getById() method.

--- src/main/java/com/Pulson/RoomReservations/controller/UserController.java
+++ src/main/java/com/Pulson/RoomReservations/controller/UserController.java
@@ -3,12 +3,10 @@ package com.Pulson.RoomReservations.controller;
 import com.Pulson.RoomReservations.model.User;
 import com.Pulson.RoomReservations.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("users")
@@ -20,6 +18,12 @@ public class UserController {
     @GetMapping("/all")
     public List<User> getAll(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") Long userId) throws Exception {
+        User user = userRepository.findById(userId).orElseThrow(() -> new Exception("User " + userId + " not found"));
+        return ResponseEntity.ok().body(user);
     }
 
 }
