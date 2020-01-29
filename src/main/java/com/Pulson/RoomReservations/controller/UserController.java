2e7e5d49a5725dca6f592b254edc69be7ed8e0fd
pulson <michal.pula98@gmail.com>
2020-01-29 13:40:46 +0100

    Add getAll() method.

--- src/main/java/com/Pulson/RoomReservations/controller/UserController.java
+++ src/main/java/com/Pulson/RoomReservations/controller/UserController.java
@@ -1,18 +1,25 @@
 package com.Pulson.RoomReservations.controller;
 
+import com.Pulson.RoomReservations.model.User;
 import com.Pulson.RoomReservations.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
-@RequestMapping(name = "users")
+@RequestMapping("users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-
+    @GetMapping("/all")
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
 
 }
