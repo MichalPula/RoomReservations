25e558a27ffd7311712fe085db029ffe54ec9ca3
pulson <michal.pula98@gmail.com>
2020-01-30 12:06:14 +0100

    Add create() method.

--- src/main/java/com/Pulson/RoomReservations/controller/ActivityController.java
+++ src/main/java/com/Pulson/RoomReservations/controller/ActivityController.java
@@ -4,10 +4,8 @@ import com.Pulson.RoomReservations.model.Activity;
 import com.Pulson.RoomReservations.model.Room;
 import com.Pulson.RoomReservations.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +26,9 @@ public class ActivityController {
         return activityRepository.findById(activityId).orElseThrow(()-> new Exception("Activity "+ activityId +" not found"));
     }
 
+    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public boolean create(@RequestBody Activity activity){
+        activityRepository.save(activity);
+        return true;
+    }
 }
