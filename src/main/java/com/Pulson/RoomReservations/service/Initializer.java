e46e6948f12fc02609ac2af127cc156a88d4abd3
pulson <michal.pula98@gmail.com>
2020-01-30 15:28:18 +0100

    Add reservations to initializer.

--- src/main/java/com/Pulson/RoomReservations/service/Initializer.java
+++ src/main/java/com/Pulson/RoomReservations/service/Initializer.java
@@ -1,16 +1,21 @@
 package com.Pulson.RoomReservations.service;
 
 import com.Pulson.RoomReservations.model.Activity;
+import com.Pulson.RoomReservations.model.Reservation;
 import com.Pulson.RoomReservations.model.Room;
 import com.Pulson.RoomReservations.model.User;
 import com.Pulson.RoomReservations.repository.ActivityRepository;
+import com.Pulson.RoomReservations.repository.ReservationRepository;
 import com.Pulson.RoomReservations.repository.RoomRepository;
 import com.Pulson.RoomReservations.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.ZonedDateTime;
+
 @Service
 public class Initializer {
-    public Initializer(UserRepository userRepository, RoomRepository roomRepository, ActivityRepository activityRepository){
+    public Initializer(UserRepository userRepository, RoomRepository roomRepository,
+                       ActivityRepository activityRepository, ReservationRepository reservationRepository){
         userRepository.save(new User("LeBron", "James", "lebron@gmail.com", 2));
         userRepository.save(new User("Kevin", "Durant", "kd@gmail.com", 1));
         userRepository.save(new User("Stephen", "Curry", "steph_curry@gmail.com", 2));
