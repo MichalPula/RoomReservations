12ce6b9f03f00bc7d730adc727fed227c559a091
pulson <michal.pula98@gmail.com>
2020-01-30 12:05:47 +0100

    Add getById() method.

--- src/main/java/com/Pulson/RoomReservations/controller/ActivityController.java
+++ src/main/java/com/Pulson/RoomReservations/controller/ActivityController.java
@@ -1,9 +1,11 @@
 package com.Pulson.RoomReservations.controller;
 
 import com.Pulson.RoomReservations.model.Activity;
+import com.Pulson.RoomReservations.model.Room;
 import com.Pulson.RoomReservations.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class ActivityController {
     @GetMapping("/all")
     public List<Activity> getAll(){
         return activityRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Activity getById(@PathVariable("id") long activityId) throws Exception {
+        return activityRepository.findById(activityId).orElseThrow(()-> new Exception("Activity "+ activityId +" not found"));
     }
 
 }
