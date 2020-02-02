6215feb778956a27e591975a3ff39cffaa72b37f
pulson <michal.pula98@gmail.com>
2020-02-02 19:52:09 +0100

    Refactor update() method- add possibility to update isAvailable.

--- src/main/java/com/Pulson/RoomReservations/service/RoomServiceImpl.java
+++ src/main/java/com/Pulson/RoomReservations/service/RoomServiceImpl.java
@@ -51,6 +51,7 @@ public class RoomServiceImpl implements RoomService{
     public Boolean update(long id, Room roomDetails) throws Exception {
         Room room = repository.findById(id).orElseThrow(() -> new Exception("Room NOT found"));
         room.setName(roomDetails.getName());
+        room.setAvailable(roomDetails.getAvailable());
         repository.save(room);
         return true;
     }
