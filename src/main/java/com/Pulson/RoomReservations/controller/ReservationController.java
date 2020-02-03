551df9cafa105020634e42e3fd61cefcfb56ba20
pulson <michal.pula98@gmail.com>
2020-02-03 12:36:39 +0100

    Add getById() method.

--- src/main/java/com/Pulson/RoomReservations/controller/ReservationController.java
+++ src/main/java/com/Pulson/RoomReservations/controller/ReservationController.java
@@ -4,6 +4,7 @@ import com.Pulson.RoomReservations.model.Reservation;
 import com.Pulson.RoomReservations.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class ReservationController {
     @GetMapping("/all")
     public List<Reservation> getAll(){
         return reservationService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Reservation getById(@PathVariable("id") long id) throws Exception {
+        return reservationService.getById(id);
     }
 }
