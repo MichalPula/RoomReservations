bec02f704069e9ba3d21445d27ff973772c1c290
pulson <michal.pula98@gmail.com>
2020-02-03 12:29:21 +0100

    Create reservation service with impl.

---
+++ src/main/java/com/Pulson/RoomReservations/service/ReservationServiceImpl.java
@@ -0,0 +1,18 @@
+package com.Pulson.RoomReservations.service;
+
+import com.Pulson.RoomReservations.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Service
+public class ReservationServiceImpl implements ReservationService{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+}
