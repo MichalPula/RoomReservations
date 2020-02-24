7e7782f7c350c657134fd07ee2162dfbe3b4641f
pulson <michal.pula98@gmail.com>
2020-02-24 10:52:11 +0100

    Change findRole method to accept RoleType.

--- src/main/java/com/Pulson/RoomReservations/repositories/RoleRepository.java
+++ src/main/java/com/Pulson/RoomReservations/repositories/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.Pulson.RoomReservations.repositories;
 
 import com.Pulson.RoomReservations.entities.Role;
+import com.Pulson.RoomReservations.entities.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findRoleByRole(String role);
+    Role findByRoleType(RoleType roleType);
 }
