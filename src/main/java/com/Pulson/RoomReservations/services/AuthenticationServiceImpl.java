6f67faebf3de1cae85e99d8c0478e40ee5d9ee8e
pulson <michal.pula98@gmail.com>
2020-02-24 15:35:28 +0100

    Add handleLogin method implementation.

--- src/main/java/com/Pulson/RoomReservations/services/AuthenticationServiceImpl.java
+++ src/main/java/com/Pulson/RoomReservations/services/AuthenticationServiceImpl.java
@@ -2,14 +2,49 @@ package com.Pulson.RoomReservations.services;
 
 import com.Pulson.RoomReservations.models.JwtLoginRequest;
 import com.Pulson.RoomReservations.models.JwtRegisterRequest;
+import com.Pulson.RoomReservations.models.JwtResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class AuthenticationServiceImpl implements AuthenticationService{
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
     @Override
     public ResponseEntity<?> handleLogin(JwtLoginRequest jwtLoginRequest) {
-        return null;
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(jwtLoginRequest.getUsername(), jwtLoginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        String token = jwtTokenService.generateToken(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(
+                token,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
     }
 
     @Override
