4ab1ee02b50c7c8459ef826fcdbdf41237b3f229
pulson <michal.pula98@gmail.com>
2020-02-28 15:42:18 +0100

    Add ReservationAddDTO class.

---
+++ src/main/java/com/Pulson/RoomReservations/entities/dtos/ReservationAddDTO.java
@@ -0,0 +1,60 @@
+package com.Pulson.RoomReservations.entities.dtos;
+
+import java.time.ZonedDateTime;
+
+public class ReservationAddDTO {
+
+    private Long userId;
+    private Long roomId;
+    private java.time.ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+    private Long activityId;
+
+    public ReservationAddDTO(Long userId, Long roomId, ZonedDateTime startTime, ZonedDateTime endTime, Long activityId) {
+        this.userId = userId;
+        this.roomId = roomId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.activityId = activityId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public ZonedDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(ZonedDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public ZonedDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(ZonedDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+}
