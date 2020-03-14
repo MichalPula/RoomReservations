7995493d6832571eb5bab8823d5d1bae397c0cda
pulson <michal.pula98@gmail.com>
2020-03-14 12:51:40 +0100

    Add CreateUpdate and Read reservation DTOs.

---
+++ src/main/java/com/Pulson/RoomReservations/entities/dtos/reservation/ReservationCreateUpdateDTO.java
@@ -0,0 +1,58 @@
+package com.Pulson.RoomReservations.entities.dtos.reservation;
+
+public class ReservationCreateUpdateDTO {
+
+    private Long userId;
+    private String roomName;
+    private String startTime;
+    private String endTime;
+    private String activityName;
+
+    public ReservationCreateUpdateDTO(Long userId, String roomName, String startTime, String endTime, String activityName) {
+        this.userId = userId;
+        this.roomName = roomName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.activityName = activityName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+}
