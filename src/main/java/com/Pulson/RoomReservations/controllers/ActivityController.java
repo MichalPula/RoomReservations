44df3df3e3f4559c2beb00e1cefcd30071d9c772
pulson <michal.pula98@gmail.com>
2020-03-16 09:06:46 +0100

    Change return values from Activity to DTO objects.

--- src/main/java/com/Pulson/RoomReservations/controllers/ActivityController.java
+++ src/main/java/com/Pulson/RoomReservations/controllers/ActivityController.java
@@ -1,33 +1,44 @@
 package com.Pulson.RoomReservations.controllers;
 
 import com.Pulson.RoomReservations.entities.Activity;
+import com.Pulson.RoomReservations.entities.dtos.activity.ActivityCreateReadUpdateDTO;
 import com.Pulson.RoomReservations.services.ActivityService;
+import com.Pulson.RoomReservations.services.mappers.activity.CreateUpdateActivityMapper;
+import com.Pulson.RoomReservations.services.mappers.activity.ReadActivityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("activities")
 public class ActivityController {
 
     @Autowired
     private ActivityService activityService;
 
-    @GetMapping("/all")
-    public List<Activity> getAll(){
-        return activityService.getAll();
+    @Autowired
+    private CreateUpdateActivityMapper createUpdateActivityMapper;
+
+    @Autowired
+    private ReadActivityMapper readActivityMapper;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ActivityCreateReadUpdateDTO> getAll(){
+        return readActivityMapper.mapToActivityCreateUpdateDTOsList(activityService.getAll());
     }
 
-    @GetMapping("/{id}")
-    public Activity getById(@PathVariable("id") long id) throws Exception {
-        return activityService.getById(id);
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActivityCreateReadUpdateDTO getById(@PathVariable("id") long id) throws Exception {
+        return readActivityMapper.mapToActivityCreateUpdateDTOsList(Arrays.asList(activityService.getById(id))).get(0);
     }
 
     @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public boolean create(@RequestBody Activity activity){
-        return activityService.create(activity);
+    public boolean create(@RequestBody ActivityCreateReadUpdateDTO activityCreateReadUpdateDTO) throws Exception {
+        return activityService.create(createUpdateActivityMapper.mapToActivity(activityCreateReadUpdateDTO));
     }
 
     @DeleteMapping("/deactivate/{id}")
@@ -35,7 +46,7 @@ public class ActivityController {
         return activityService.deactivate(id);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping(value = "/update/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public boolean update(@PathVariable("id") long id, @RequestBody Activity activityDetails) throws Exception {
         return activityService.update(id, activityDetails);
     }
