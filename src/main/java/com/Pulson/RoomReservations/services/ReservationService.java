a59285ae022ca678b2d4c50936f34574f225340f
pulson <michal.pula98@gmail.com>
2020-03-16 09:35:40 +0100

    Add getStartingHoursListByDate, getHistoryByUser, getAmountByDateByUser and getActiveByUser methods.

--- src/main/java/com/Pulson/RoomReservations/services/ReservationService.java
+++ src/main/java/com/Pulson/RoomReservations/services/ReservationService.java
@@ -8,10 +8,13 @@ import java.util.List;
 @Service
 public interface ReservationService {
     List<Reservation> getAll();
-    List<Reservation> getByUser(long userId) throws Exception;
+    List<Reservation> getActive();
+    List<Reservation> getActiveByUser(long userId) throws Exception;
+    List<Reservation> getHistoryByUser(long userId) throws Exception;
+    List<Integer> getStartingHoursListByDate(int year, int month, int day) throws Exception;
+    Integer getAmountByDateByUser(int year, int month, int day, long userId) throws Exception;
     Reservation getById(long id) throws Exception;
     Boolean create(Reservation reservation) throws Exception;
     Boolean update(long id, Reservation reservation) throws Exception;
     Boolean delete(long id) throws Exception;
-    Boolean update(long id, Reservation reservation) throws Exception;
 }
