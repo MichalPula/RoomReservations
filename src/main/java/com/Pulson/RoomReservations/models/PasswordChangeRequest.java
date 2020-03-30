7627f10d7a14605f0c41ae061f59f37575ddbc94
pulson <michal.pula98@gmail.com>
2020-03-30 11:25:28 +0200

    Add PasswordChangeRequest model.

---
+++ src/main/java/com/Pulson/RoomReservations/models/PasswordChangeRequest.java
@@ -0,0 +1,30 @@
+package com.Pulson.RoomReservations.models;
+
+import java.io.Serializable;
+
+public class PasswordChangeRequest implements Serializable {
+
+    private long userId;
+    private String oldPassword;
+    private String newPassword;
+
+    public PasswordChangeRequest(long userId, String oldPassword, String newPassword) {
+        this.userId = userId;
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+    }
+
+    public PasswordChangeRequest() {}
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+}
