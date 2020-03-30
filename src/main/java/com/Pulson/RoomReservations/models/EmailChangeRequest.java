8a6b1f8b922645bfc6e7ffa877c2fc02ed5e4ebf
pulson <michal.pula98@gmail.com>
2020-03-30 11:25:46 +0200

    Add EmailChangeRequest model.

---
+++ src/main/java/com/Pulson/RoomReservations/models/EmailChangeRequest.java
@@ -0,0 +1,24 @@
+package com.Pulson.RoomReservations.models;
+
+import java.io.Serializable;
+
+public class EmailChangeRequest implements Serializable {
+
+    private long userId;
+    private String email;
+
+    public EmailChangeRequest(long userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+    public EmailChangeRequest() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+}
