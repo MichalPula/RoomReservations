e31ae1c8df763169b745514e1fb1d46858dcdc01
pulson <michal.pula98@gmail.com>
2020-03-30 11:22:13 +0200

    Add update password/email and get basic account data methods.

--- src/main/java/com/Pulson/RoomReservations/services/UserService.java
+++ src/main/java/com/Pulson/RoomReservations/services/UserService.java
@@ -1,6 +1,10 @@
 package com.Pulson.RoomReservations.services;
 
 import com.Pulson.RoomReservations.entities.User;
+import com.Pulson.RoomReservations.models.BasicAccountDataChangeRequest;
+import com.Pulson.RoomReservations.models.EmailChangeRequest;
+import com.Pulson.RoomReservations.models.PasswordChangeRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,9 @@ public interface UserService extends UserDetailsService {
     User getById(long id) throws Exception;
     Boolean create(User user);
     Boolean deactivate(long id) throws Exception;
-    Boolean update(long id, User userDetails) throws Exception;
+    Boolean updateBasicInfo(BasicAccountDataChangeRequest basicAccountInfo) throws Exception;
+    Boolean updateEmail(EmailChangeRequest emailChangeRequest) throws Exception;
+    ResponseEntity<?> updatePassword(PasswordChangeRequest passwordChangeRequest) throws Exception;
     Boolean existsByUsername(String username);
+    BasicAccountDataChangeRequest getBasicAccountData(long id);
 }
--- src/main/java/com/Pulson/RoomReservations/services/UserServiceImpl.java
+++ src/main/java/com/Pulson/RoomReservations/services/UserServiceImpl.java
@@ -1,26 +1,34 @@
 package com.Pulson.RoomReservations.services;
 
 import com.Pulson.RoomReservations.entities.User;
+import com.Pulson.RoomReservations.models.BasicAccountDataChangeRequest;
+import com.Pulson.RoomReservations.models.EmailChangeRequest;
+import com.Pulson.RoomReservations.models.PasswordChangeRequest;
 import com.Pulson.RoomReservations.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @PersistenceContext
-    private EntityManager em;
+    private EntityManager entityManager;
+    private UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    public UserServiceImpl(EntityManager entityManager, UserRepository userRepository) {
+        this.entityManager = entityManager;
+        this.userRepository = userRepository;
+    }
 
     @Override
     public List<User> getAll() {
@@ -42,7 +50,7 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public Boolean deactivate(long id) {
-        Query query = em.createNativeQuery("update users set is_active = false where id = ?");
+        Query query = entityManager.createNativeQuery("update users set is_active = false where id = ?");
         query.setParameter(1, id);
         query.executeUpdate();
         return true;
@@ -50,17 +58,51 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     @Override
-    public Boolean update(long id, User userDetails) throws Exception {
-//        User user = userRepository.findById(id).orElseThrow(() -> new Exception("User NOT found"));
-//        user.setUsername(userDetails.getUsername());
-//        user.setEnabled(userDetails.getEnabled());
-//        userRepository.save(user);
+    public Boolean updateBasicInfo(BasicAccountDataChangeRequest basicAccountInfo) throws Exception {
+        User user = userRepository.findById(basicAccountInfo.getUserId()).orElseThrow(() -> new Exception("User NOT found"));
+        user.setFirstName(basicAccountInfo.getFirstName());
+        user.setLastName(basicAccountInfo.getLastName());
+        user.setPhoneNumber(basicAccountInfo.getPhoneNumber());
+        userRepository.save(user);
         return true;
+    }
+
+    @Override
+    public Boolean updateEmail(EmailChangeRequest emailChangeRequest) throws Exception {
+        User user = userRepository.findById(emailChangeRequest.getUserId()).orElseThrow(() -> new Exception("User NOT found"));
+        user.setUsername(emailChangeRequest.getEmail());
+        userRepository.save(user);
+        return true;
+    }
+
+    @Override
+    public ResponseEntity<?> updatePassword(PasswordChangeRequest passwordChangeRequest) throws Exception {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        User user = userRepository.findById(passwordChangeRequest.getUserId()).orElseThrow(() -> new Exception("User NOT found"));
+        if(!bCryptPasswordEncoder.matches(passwordChangeRequest.getOldPassword(), user.getPassword())){
+            return new ResponseEntity<>("Wrong password", HttpStatus.CONFLICT);
+        } else {
+            user.setPassword(bCryptPasswordEncoder.encode(passwordChangeRequest.getNewPassword()));
+            userRepository.save(user);
+        }
+        return new ResponseEntity<>("Password changed", HttpStatus.OK);
     }
 
     @Override
     public Boolean existsByUsername(String username) {
         return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public BasicAccountDataChangeRequest getBasicAccountData(long userId) {
+        Query query = entityManager.createNativeQuery("select" +
+                " first_name, last_name, phone_number" +
+                " from users" +
+                " where id = ?");
+        query.setParameter(1, userId);
+        List<Object[]> resultList = query.getResultList();
+        return new BasicAccountDataChangeRequest(resultList.get(0)[0].toString(),
+                resultList.get(0)[1].toString(), Long.parseLong(resultList.get(0)[2].toString()));
     }
 
     @Override
@@ -72,4 +114,6 @@ public class UserServiceImpl implements UserService {
             throw new UsernameNotFoundException("User Not Found with username: " + username);
         }
     }
+
+
 }
