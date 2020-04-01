8dffe03a97c52ffa967d32bba239c2c709bca653
pulson <michal.pula98@gmail.com>
2020-04-01 09:43:56 +0200

    Add html page with corresponding TypeScript methods listing all rooms with ability do deactivate and add room.

--- frontend/src/app/boards/common/rooms/rooms.component.ts
+++ frontend/src/app/boards/common/rooms/rooms.component.ts
@@ -1,31 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Room, UserService} from '../../../services/user.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {AdminService} from '../../../services/admin.service';
+import {NG_VALIDATORS} from '@angular/forms';
+import {RoomNameValidateDirective} from './room-name-validator';
+
+export interface RoomAddUpdateForm {
+  name: string;
+  available: boolean;
+}
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  styleUrls: ['./rooms.component.css'],
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: RoomNameValidateDirective,
+    multi: true
+  }]
 })
 
 export class RoomsComponent implements OnInit {
 
-  public rooms: Room[];
+  rooms: Room[];
 
-  constructor(private userService: UserService) { }
+  roomAddUpdateForm: RoomAddUpdateForm = {
+    name: '',
+    available: null
+  };
+
+  constructor(private adminService: AdminService, private userService: UserService, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.fetchData();
   }
 
   fetchData() {
-    this.userService.getAllRooms().subscribe(data => {
+    this.adminService.getAllRooms().subscribe(data => {
       this.rooms = data as Room[];
     });
   }
 
   deactivateRoom(roomId: number) {
-    this.userService.deactivateRoom(roomId).subscribe(data => {
+    this.adminService.deactivateRoom(roomId).subscribe(data => {
       this.fetchData();
     });
+  }
+
+  openAddRoomModal(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  newRoomFromSubmit() {
+    this.adminService.addRoom(this.roomAddUpdateForm).subscribe(data => {
+      this.refreshPage();
+    });
+  }
+
+  refreshPage() {
+    window.location.replace('/rooms');
   }
 }
