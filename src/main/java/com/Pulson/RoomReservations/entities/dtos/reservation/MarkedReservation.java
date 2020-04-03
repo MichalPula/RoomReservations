ee1c1ca5994c264e5bfce169de340268ee69251a
pulson <michal.pula98@gmail.com>
2020-04-03 14:16:08 +0200

    Add MarkedReservation model.

---
+++ src/main/java/com/Pulson/RoomReservations/entities/dtos/reservation/MarkedReservation.java
@@ -0,0 +1,32 @@
+package com.Pulson.RoomReservations.entities.dtos.reservation;
+
+public class MarkedReservation {
+
+    private ReservationReadDTO reservation;
+    private String color;
+
+    public MarkedReservation(ReservationReadDTO reservation, String color) {
+        this.reservation = reservation;
+        this.color = color;
+    }
+
+    public MarkedReservation() {
+
+    }
+
+    public ReservationReadDTO getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(ReservationReadDTO reservation) {
+        this.reservation = reservation;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
