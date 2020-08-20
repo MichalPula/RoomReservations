63226ea94bd7e22646c16710c481694df2f17702
pulson <michal.pula98@gmail.com>
2020-08-20 13:33:06 +0200

    Add ErrorMessage and GlobalExceptionHandler class.

---
+++ src/main/java/com/Pulson/RoomReservations/exception_handler/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.Pulson.RoomReservations.exception_handler;
+
+import com.Pulson.RoomReservations.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({UserNotFoundException.class})
+    public ResponseEntity<ErrorMessage> handleResourceNotFound(Exception exception, WebRequest request){
+        return new ResponseEntity<>(new ErrorMessage(
+                        LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), exception.getMessage()),
+                        HttpStatus.NOT_FOUND);
+    }
+}
